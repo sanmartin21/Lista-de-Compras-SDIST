6f33749ab3b3a0a25f5f9f9de912e9cf66feac88
sanmartin21 <jvsanmartin@yahoo.com.br>
2022-05-20 00:17:17 -0300

    update

--- app/view/ViewUpdate.php
+++ app/view/ViewUpdate.php
@@ -16,10 +16,10 @@ class ViewUpdate extends ViewPadrao
             <legend>Alteração de Produtos: </legend></br></br>
             <form action="index.php" method="GET">
                 <input type="hidden" name="prodcodigo" value = "'.$_GET['prodcodigo'].'">
-                <input required type="text" name="prodnome" class="form-control" placeholder="Insira o nome do Produto:" aria-label="First name"></br></br>
-                <input required type="number" name="prodpreco" class="form-control" placeholder="Insira o Preço do Produto:" aria-label="First name"></br></br>
+                <input required type="text" name="prodnome" class="form-control" placeholder="Alterar o nome do Produto:" aria-label="First name"></br></br>
+                <input required type="number" name="prodpreco" class="form-control" placeholder="Alterar o Preço do Produto:" aria-label="First name"></br></br>
                 <div class="form-group shadow-textarea">
-                <textarea name="proddescricao" class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="3" placeholder="Insira a Descrição do Produto"></textarea>
+                <textarea name="proddescricao" class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="8" placeholder="Alterar a Descrição do Produto:"></textarea>
                 </div>
                 <input type="hidden" name="pg" value="update"><br>
                 <input type="hidden" name="act" value="update"><br>
